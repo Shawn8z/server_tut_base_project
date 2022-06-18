db54ea64eb659e4bcbefa71792a175aa5251b236
Shawn <lcorel86@gmail.com>
2022-06-18 02:13:55 -0500

    back end done on tut

---
+++ src/main/java/ig/getarrays/server/repo/ServerRepo.java
@@ -0,0 +1,9 @@
+package ig.getarrays.server.repo;
+
+import ig.getarrays.server.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepo extends JpaRepository<Server, Long> {
+
+    Server findByIpAddress(String ipAddress);
+}
