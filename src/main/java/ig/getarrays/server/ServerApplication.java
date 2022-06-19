b59adf88d0d5e9063539db65073c3bbd56cf29b1
Shawn <lcorel86@gmail.com>
2022-06-19 17:18:00 -0500

    added corsFilter to make backend and frontend linkable

--- src/main/java/ig/getarrays/server/ServerApplication.java
+++ src/main/java/ig/getarrays/server/ServerApplication.java
@@ -4,10 +4,18 @@ package ig.getarrays.server;
 import ig.getarrays.server.enumeration.Status;
 import ig.getarrays.server.model.Server;
 import ig.getarrays.server.repo.ServerRepo;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+import java.util.Arrays;
 
 import static ig.getarrays.server.enumeration.Status.SERVER_UP;
 
@@ -61,5 +69,29 @@ public class ServerApplication {
 							"http://localhost:8080/server/image/server4.png", Status.SERVER_DOWN)
 			);
 		};
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+//
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(
+				Arrays.asList("origin", "Access-Control-Allow-Origin", "Content-Type", "Accept",
+				"Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request-Method",
+				"Access-Control-Request-Headers"));
+
+		corsConfiguration.setExposedHeaders(
+				Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token",
+				"Authorization", "Access0Control-Allow-Origin", "Access-Control-Allow-Origin",
+				"Access-Control-Allow-Credentials", "Filename"));
+
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+//
+		source.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(source);
+
 	}
 }
